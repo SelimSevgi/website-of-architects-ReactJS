b77e6cf7f0a4938aacf98011ee7682f08e8d44d8
SelimSevgi <slmsvg44@gmail.com>
2021-12-07 22:00:39 +0300

    :pencil2: proje sayfasına modal eklendi

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,86 +2,21 @@ import React, { useState } from "react";
 import PureModal from "react-pure-modal";
 import "react-pure-modal/dist/react-pure-modal.min.css";
 
-export default function Projects() {
-  const [modal, setModal] = useState(false);
+export default function Modal(props) {
+  const { modal } = props.modal;
+  console.log(props.modal);
+
+  const [modal1, setModal1] = useState(props.modal);
   return (
-    <div className="pro">
-      <div className="pro-header">
-        <h1>
-          <span className="pro-header-gri">Our</span>
-          <br />
-          Projects
-        </h1>
-      </div>
-      <div className="pro-list">
-        <div className="pro-list-item">
-          <div className="pro-list-right">
-            <img src="../images/project-1.png" />
-          </div>
-          <div className="pro-list-left">
-            <h2>Sample Project</h2>
-            <div className="pro-list-p">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-            <div className="pro-list-btn">
-              <button onClick={() => setModal(true)}>VIEW MORE</button>{" "}
-            </div>
-          </div>
-        </div>
-
-        <div className="pro-list-item">
-          <div className="pro-list-right">
-            <img src="../images/project-2.png" />
-          </div>
-          <div className="pro-list-left">
-            <h2>Sample Project</h2>
-            <div className="pro-list-p">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-            <div className="pro-list-btn">
-              <button>VIEW MORE</button>{" "}
-            </div>
-          </div>
-        </div>
-
-        <div className="pro-list-item">
-          <div className="pro-list-right">
-            <img src="../images/project-3.png" />
-          </div>
-          <div className="pro-list-left">
-            <h2>Sample Project</h2>
-            <div className="pro-list-p">
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book.
-              </p>
-            </div>
-            <div className="pro-list-btn">
-              <button>VIEW MORE</button>{" "}
-            </div>
-          </div>
-        </div>
-      </div>
+    <div>
       <PureModal
         width="1440px"
         height="5000px"
-        isOpen={modal}
+        isOpen={modal1}
         closeButton="close"
         closeButtonPosition="bottom"
         onClose={() => {
-          setModal(false);
+          setModal1(false);
           return true;
         }}
       >
@@ -136,7 +71,6 @@ export default function Projects() {
           </div>
         </div>
       </PureModal>
-      ;
     </div>
   );
 }
